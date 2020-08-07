fe37ceafe90a50a7ea4da9db6698b5c34f250025
Arpit Paliwal <apaliwal@localhost.localdomain>
2020-08-07 18:46:02 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  useEffect(()=>{
+    load()
+  }, [])
+
+  async function load() {
+    try{
+      let { status } = await Location.requestPermissionsAsync();
+      if(status != 'granted'){
+        setErrorMessage("Access to location need to run app");
+        return 
+      }
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+      const {latitude,longitude} = location.coords
+      alert(`longitude: ${longitude}, latitude: ${latitude}`)
+    }catch(err){}
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
